9d1a45dfb8af49da58031fa5fc965718522185b1
arshkhatwani <arsh.khatwani@gmail.com>
2024-03-08 18:58:50 +0530

    feat: add model invocation in api route

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, jsonify, make_response, request
+import boto3
+from services.ticket_description_generator import TicketDescriptionGenerator
 
 app = Flask(__name__)
+client = boto3.client('bedrock-runtime')
 
 
 @app.route("/generate")
 def hello_from_root():
-    return jsonify(message='Hello from generate')
+    prompt = request.args.get('prompt')
+
+    ticket_description_generator = TicketDescriptionGenerator(client)
+    response = ticket_description_generator.generate_description(prompt)
+
+    return jsonify(message=response)
 
 
 @app.errorhandler(404)
