9825ca6302b8a26275f4aeda267b8327aefea6df
arshkhatwani <arsh.khatwani@gmail.com>
2024-03-08 18:41:26 +0530

    feat: add claude model class

---
+++ ai_models/claude_v3.py
@@ -0,0 +1,41 @@
+import json
+
+
+class ClaudeV3:
+    def __init__(self, client):
+        self.model_id = "anthropic.claude-3-sonnet-20240229-v1:0"
+        self.client = client
+
+    def get_body(self, system_role: str, prompt: str):
+        body = {
+            "anthropic_version": "bedrock-2023-05-31",
+            "max_tokens": 1024,
+            "system": system_role,
+            "messages": [
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "text",
+                            "text": prompt
+                        }
+                    ]
+                }
+            ]
+        }
+        return json.dumps(body)
+
+    def invoke_model(self, system_role: str, prompt: str):
+        body = self.get_body(system_role, prompt)
+
+        try:
+            response = self.client.invoke_model(
+                modelId=self.model_id,
+                body=body
+            )
+            response_body = json.loads(response["body"].read())
+        except Exception as e:
+            print("Couldn't invoke Anthropic Claude due to exception:", str(e))
+            response_body = {}
+
+        return response_body
