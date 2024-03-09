69f78f4a2a22f1fda42f3f54bee4128f9d198c77
arshkhatwani <arsh.khatwani@gmail.com>
2024-03-09 22:00:30 +0530

    feat: add app title

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,9 +1,11 @@
 import PromptInput from "./components/PromptInput";
 import PromptOutput from "./components/PromptOutput";
+import Title from "./components/Title";
 
 function App() {
     return (
         <div className="min-h-screen flex flex-col">
+            <Title />
             <PromptInput />
             <PromptOutput />
         </div>
---
+++ ui/src/components/Title.tsx
@@ -0,0 +1,7 @@
+export default function Title() {
+    return (
+        <h1 className="my-5 text-5xl font-bold text-center title-heading">
+            Agile Ticket Description Generator
+        </h1>
+    );
+}
