e2d0c514ee5739fd11b3f9cb50f6f4c4bfaab87b
arshkhatwani <arsh.khatwani@gmail.com>
2024-03-09 17:50:15 +0530

    feat: add markdown in output

--- ui/src/components/PromptOutput.tsx
+++ ui/src/components/PromptOutput.tsx
@@ -1,12 +1,15 @@
 import { useRecoilState } from "recoil";
 import { promptOutputState } from "../state/atoms/prompt";
+import Markdown from "react-markdown";
 
 export default function PromptOutput() {
     const [output] = useRecoilState(promptOutputState);
 
     return (
         <div className="mt-6 w-full flex justify-center">
-            <p className="w-5/6 text-xl">{output}</p>
+            <p className="w-5/6 text-xl">
+                <Markdown>{output}</Markdown>
+            </p>
         </div>
     );
 }
