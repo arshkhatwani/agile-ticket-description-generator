5bb037e4bd8f10828b2a833e4a25853d56b6475d
arshkhatwani <arsh.khatwani@gmail.com>
2024-03-16 17:09:42 +0530

    refactor: replace print with logger

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, jsonify, make_response, request
 import boto3
+import logging
 from services.ticket_description_generator import TicketDescriptionGenerator
+
+logger = logging.getLogger()
+logger.setLevel("INFO")
 
 app = Flask(__name__)
 client = boto3.client('bedrock-runtime')
@@ -12,9 +16,9 @@ def generate_description():
     ticket_type = request.json.get('ticket_type', 'story')
     additional_details = request.json.get('additional_details', False)
 
-    print('prompt:', prompt)
-    print('ticket_type:', ticket_type)
-    print('additional_details:', additional_details)
+    logger.info('prompt: %s', prompt)
+    logger.info('ticket_type: %s', ticket_type)
+    logger.info('additional_details: %s', additional_details)
 
     ticket_description_generator = TicketDescriptionGenerator(client)
     response = ticket_description_generator.generate_description(
