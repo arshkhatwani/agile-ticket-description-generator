cbb930d90fe8672c9fba9626f73d30f644fb9034
arshkhatwani <arsh.khatwani@gmail.com>
2024-03-19 00:57:41 +0530

    feat: add TemplateInput in PromptInput

--- ui/src/components/Prompt/PromptInput.tsx
+++ ui/src/components/Prompt/PromptInput.tsx
@@ -1,9 +1,11 @@
 import { useRecoilState } from "recoil";
 import {
     additionalDetailsState,
+    includeTemplateState,
     promptInputState,
     promptLoadingState,
     promptOutputState,
+    templateInputState,
     ticketTypeState,
 } from "../../state/atoms/prompt";
 import generateDescription from "../../api/generateDescription";
@@ -12,7 +14,7 @@ import AdditonalDetails from "./AdditonalDetails";
 import PromptInputTooltip from "./PromptInputTooltip";
 import iconPaths from "../../constants/iconPaths";
 import SubmitBtnLoading from "../Loading/SubmitBtnLoading";
-import TemplateInput from "../TemplateInput";
+import TemplateInput from "../TicketTemplate/TemplateInput";
 import Textarea from "../Inputs/Textarea";
 
 export default function PromptInput() {
@@ -21,21 +23,28 @@ export default function PromptInput() {
     const [loading, setLoading] = useRecoilState(promptLoadingState);
     const [ticketType] = useRecoilState(ticketTypeState);
     const [additionalDetails] = useRecoilState(additionalDetailsState);
+    const [includeTemplate] = useRecoilState(includeTemplateState);
+    const [templateInput] = useRecoilState(templateInputState);
 
     const onSubmit = async () => {
         if (input.trim() === "") return;
         if (loading) return;
 
         setLoading(true);
+
+        const template = includeTemplate ? templateInput : "";
         const description = await generateDescription(
             input,
             ticketType,
-            additionalDetails
+            additionalDetails,
+            template
         );
+
         if (!description) {
             alert("Could not generate description, please try later");
             return;
         }
+
         setOutput(description);
         setLoading(false);
     };
@@ -44,6 +53,7 @@ export default function PromptInput() {
         <div className="w-full flex flex-col items-center my-3">
             <div className="w-[80%] flex gap-2 input-container rounded-lg p-2">
                 <Textarea
+                    className="flex-1"
                     placeholder="Enter your ticket details"
                     rows={1}
                     value={input}
