59eb08b18c8b8ba3725a9e62d9ef9017a2165a22
shivampatel2719 <shivam.160410107094@gmail.com>
2024-03-20 13:55:39 +0530

    removes unncecessary changes

--- ui/src/components/PromptOutput.tsx
+++ ui/src/components/PromptOutput.tsx
@@ -1,7 +1,7 @@
-import { useRecoilState } from 'recoil';
-import { promptLoadingState, promptOutputState } from '../state/atoms/prompt';
-import Markdown from 'react-markdown';
-import Loading from './Loading/Loading';
+import { useRecoilState } from "recoil";
+import { promptLoadingState, promptOutputState } from "../state/atoms/prompt";
+import Markdown from "react-markdown";
+import Loading from "./Loading/Loading";
 
 export default function PromptOutput() {
     const [output] = useRecoilState(promptOutputState);
@@ -17,13 +17,11 @@ export default function PromptOutput() {
 
     return (
         <div className="mt-6 w-full flex justify-center ">
-            <div className="w-[80%]">
-                <p className="text-xl py-2 px-3 rounded-lg output-container">
-                    <Markdown className="prose prose-lg prose-invert">
-                        {output}
-                    </Markdown>
-                </p>
-            </div>
+            <p className="w-[80%] text-xl py-2 px-3 rounded-lg output-container">
+                <Markdown className="prose prose-lg prose-invert">
+                    {output}
+                </Markdown>
+            </p>
         </div>
     );
 }
